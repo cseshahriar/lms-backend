c89297f942870d1638047335bfcab5a5040118bf
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-20 22:17:30 +0600

    teacher seriaizer

---
+++ teachers/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Teacher
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Teacher
+        fields = (
+            'ull_name', 'email', 'password', 'qualification',
+            'mobile_no', 'address',
+        )
