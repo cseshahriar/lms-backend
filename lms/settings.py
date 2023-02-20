4c3b985d7aaf669778f9c044f62bafba5a497613
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-20 23:11:51 +0600

    auth token setup

--- lms/settings.py
+++ lms/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # third party libraries
     'rest_framework',
     'django_extensions',
+    'rest_framework.authtoken',
 
     # custom apps
     'teachers',
@@ -132,3 +133,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
