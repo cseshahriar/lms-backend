ea5af23502e68b47538f4391de0897b1fc9bb932
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-03-09 14:43:33 +0600

    student login validation fix

--- students/views.py
+++ students/views.py
@@ -8,6 +8,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from .serializers import (
     StudentSerializer
 )
@@ -48,6 +50,29 @@ class StudentLoginAPIView(APIView):
     def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
+        if email is None or email == '':
+            return Response(
+                {'error': 'Email is required.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            validate_email(email)
+        except ValidationError as e:
+            return Response({'email': e.message}, status=400)
+
+        if password is None or password == '':
+            return Response(
+                {'error': 'Password is required.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if password and len(password) < 7:
+            return Response(
+                {'error': 'Password length minimum 7 characters.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         teacher = Student.objects.filter(email=email).first()
         if teacher:
             if check_password(password, teacher.password):
